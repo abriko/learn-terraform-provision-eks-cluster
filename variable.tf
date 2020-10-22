0303555bc684dce974a0d8fec69b19e835ad7f47
Abriko <i@hupo.me>
2020-10-22 14:40:32 +0800

    change var

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  default     = "us-east-2"
+  description = "AWS region"
+}
+
+variable "project_name" {
+  default     = "training"
+  description = "Project name"
+}
--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,13 @@
-variable "region" {
-  default     = "us-east-2"
-  description = "AWS region"
-}
 
 provider "aws" {
   version = ">= 2.28.1"
-  region  = "us-east-2"
+  region  = var.ragion
 }
 
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "training-eks-${random_string.suffix.result}"
+  cluster_name = "${var.project_name}-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -23,7 +19,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.6.0"
 
-  name                 = "training-vpc"
+  name                 = "${var.project_name}-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
