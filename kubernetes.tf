1a5767015f1f5ea31e45dce3eac3c21336efabb5
Abriko <i@hupo.me>
2020-10-24 15:31:22 +0800

    add deploy rancher server

--- kubernetes.tf
+++ kubernetes.tf
@@ -8,3 +8,30 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 }
+
+resource "kubernetes_namespace" "cattle-system" {
+  metadata {
+    name = "cattle-system"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    load_config_file       = "false"
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  }
+}
+
+resource "helm_release" "rancher-server" {
+  name  = "rancher"
+  chart = "rancher-latest/rancher"
+  repository = "https://releases.rancher.com/server-charts/latest"
+  namespace = "cattle-system"
+
+  set {
+    name  = "hostname"
+    value = data.aws_eks_cluster.cluster.endpoint
+  }
+}
