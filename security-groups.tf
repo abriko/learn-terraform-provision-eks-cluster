d6866627576d0849f63b6e0b71c014e541fe1539
Abriko <i@hupo.me>
2020-10-25 10:12:34 +0800

    add sg rules

--- security-groups.tf
+++ security-groups.tf
@@ -45,3 +45,15 @@ resource "aws_security_group" "all_worker_mgmt" {
     ]
   }
 }
+
+resource "aws_security_group_rule" "all_worker_mgmt_rules" {
+  count = length(var.sg_ingress_rules)
+
+  type              = "ingress"
+  from_port         = var.sg_ingress_rules[count.index].from_port
+  to_port           = var.sg_ingress_rules[count.index].to_port
+  protocol          = var.sg_ingress_rules[count.index].protocol
+  cidr_blocks       = var.sg_ingress_rules[count.index].cidr_block
+  description       = var.sg_ingress_rules[count.index].description
+  security_group_id = aws_security_group.all_worker_mgmt.id
+}
--- variables.tf
+++ variables.tf
@@ -14,3 +14,29 @@ variable "eks_version" {
   type        = number
   description = "EKS cluster version"
 }
+
+variable "sg_ingress_rules" {
+    type = list(object({
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_block  = array
+      description = string
+    }))
+    default     = [
+        {
+          from_port   = 80
+          to_port     = 80
+          protocol    = "tcp"
+          cidr_block  = ["0.0.0.0/0"]
+          description = "http"
+        },
+        {
+          from_port   = 443
+          to_port     = 443
+          protocol    = "tcp"
+          cidr_block  = ["0.0.0.0/0"]
+          description = "https"
+        },
+    ]
+}
